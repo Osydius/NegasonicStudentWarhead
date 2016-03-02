59fb4fc8d5ed471a36c71344a284a493996e4b85
James Hall <james.hall@hotmail.com>
2016-03-02 16:39:33 +0000

    New server with config and basic twitter api calls
    New server that returns tweets when sent a set of JSON.
    Finds tweets based on all information (AND) not (OR).

---
+++ Server/config.js
@@ -0,0 +1,20 @@
+/*
+*
+* Authors: James Hall and Nicola Willis
+* Team: NegasonicStudentWarhead
+*
+*/
+var config = {};
+
+config.portNo = 3000;
+
+config.twitter = {
+	consumer_key: 'dtKIsNlfcoMSFYG2GtrrgIeC6',
+	consumer_secret: 'Z0odfical3CpTTnzBuF0xYIC4ArLB7d1KU8wrun53XtXKLkDIK',
+	access_token: '703233270037811201-Qv8OaglDoBgFjvBfgMALF7IogGt6HoA',
+	access_token_secret: 'NRk6oTICWLfAYaoLEDXuyq1AHmx4SaKEcLCR7XsnmHsnU'
+};
+
+config.mysql = {};
+
+module.exports = config;
---
+++ Server/server.js
@@ -0,0 +1,103 @@
+/*
+*
+* Authors: James Hall and Nicola Willis
+* Team: NegasonicStudentWarhead
+*
+*/
+
+var protocol = require('http');
+var static = require('node-static');
+var util = require('util');
+var url = require('url');
+var querystring = require('querystring');
+var Twit = require('twit');
+var config = require('./config.js');
+
+var twitterClient = new Twit(config.twitter);
+ 
+var fileServer = new (static.Server)();
+var portNo = config.portNo;
+
+var serverApp = protocol.createServer(function (request, response) {
+	var pathname = url.parse(request.url).pathname;
+
+	if(request.method == 'GET'){
+		if(pathname != "" && pathname != null && pathname != "/"){
+			console.log("getting methods");
+		} else {
+			callError(request, response);
+		}
+	} else if (request.method == 'POST'){
+		if(pathname != "" && pathname != null && pathname != "/"){
+			var body = '';
+      req.on('data', function (data) {
+          body += data;
+          if (body.length > 1e6) {
+              res.writeHead(413, {'Content-Type': 'text/plain'}).end();
+              req.connection.destroy();
+          }
+      });
+      req.on('end', function () {
+      	if(pathname == '/getAllTweets.html'){
+					getAllTweets(body, response);
+				} else if(pathname == '/getAnyTweets.html'){
+					getAnyTweets(body);
+				}
+      });
+		} else {
+			callError(request, response);
+		}
+	} else {
+		callError(request, response);
+	}
+}).listen(config.portNo);
+
+function callError(request, response){
+	fileServer.serve(request, response, function (error, result) {
+    if (error != null) {
+        console.error('Error serving %s - %s', request.url, error.message);
+        if (error.status === 404 || error.status === 500) {
+            fileServer.serveFile(util.format('error_pages/%d.html', error.status), error.status, {}, request, response);
+        } else {
+            response.writeHead(error.status, error.headers);
+            response.end();
+        }
+    }
+	});
+}
+
+function getAllTweets(clientData, response){
+	var allData = JSON.parse(clientData);
+
+	var twitterQuery = '';
+	var queryTeam = alLData.team;
+	var queryPlayers = allData.players;
+	var queryHashtags = allData.hashtags;
+	var queryKeywords = allData.keywords;
+
+	twitterQuery = twitterQuery + ' from:' + queryTeam;
+	$.each(queryPlayers, function(playerIndex, player){
+		twitterQuery = twitterQuery + ' from:' + player;
+	});
+
+	$.each(queryHashtags, function(hashtagIndex, hashtag){
+		twitterQuery = twitterQuery + ' #' + hashtag;
+	});
+
+	$.each(queryPlayers, function(keywordIndex, keyword){
+		twitterQuery = twitterQuery + player;
+	});
+
+	twitterClient.get('search/tweets', { q: twitterQuery }, function(err, data, response) {
+
+    tweets = JSON.stringify(data.statuses);
+
+    response.writeHead(200, {"Content-Type": "application/json", 'Access-Control-Allow-Origin': '*'});
+    response.end(tweets);
+
+  });
+}
+
+function getAnyTweets(clientData){
+
+}
