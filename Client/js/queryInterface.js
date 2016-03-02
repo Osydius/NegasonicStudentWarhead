9fd90c7fa0ec6567e76cbc9f1f373791945e1eda
Nicola Willis <n.c.willis@ntlworld.com>
2016-03-02 16:37:43 +0000

    Client Side set up
    Set up client side form, start of basic JQuery calls to server

---
+++ Client/js/queryInterface.js
@@ -0,0 +1,55 @@
+console.log('inside js')
+
+var sendALLButton = document.getElementById('sendALLButton');
+sendALLButton.onclick = sendData;
+
+var sendANYButton = document.getElementById('sendANYButton');
+sendANYButton.onclick = sendData;
+
+	function sendData() {
+        var id = event.target.id;
+        if(id == "sendALLButton"){
+        	var form = $('#myForm')
+        	sendALLAjaxQuery('http://localhost:3000/', JSON.stringify($('form').serializeObject()));
+        	
+        } else {
+        	console.log("ANY")
+        }
+        return false;
+	}
+
+
+
+	function sendALLAjaxQuery(url, data) {
+        console.log('sendAjaxQuery ' + url);
+        $.ajax({
+			dataType: 'json',
+			data: data,
+            type: 'POST',
+            url: url + 'getAllTweets.html',
+            success: function (data, status, xhr) {
+                console.log(data)
+            },
+            error: function (xhr, status, error) {
+                console.log('Error: ' + error.message);
+            }
+		});
+    }
+
+
+    function serializeObject() {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function () {
+            if (o[this.name] !== undefined) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+
